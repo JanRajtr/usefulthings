86b7fc07a88ef418357d96e4e41432d7d55f172b
Jan Rajtr <rajtrjan@gmail.com>
2019-06-21 18:14:30 +0200

    + methods of array extensions are public

--- Sources/UsefulThings/Extensions/Array.swift
+++ Sources/UsefulThings/Extensions/Array.swift
@@ -13,7 +13,7 @@ infix operator ====:ComparisonPrecedence
 public extension Array where Element: Comparable {
     
     /// Sorts the array. The affects the order of the array.
-    mutating func sort() {
+    public mutating func sort() {
         self = self.sorted()
     }
 }
@@ -22,23 +22,23 @@ public extension Array where Element: Comparable {
 public extension Array where Element : Hashable {
     
     /// Removes the duplicates from the array. The function is mutating and affects the order of the array.
-    mutating func removeDuplicates() {
+    public mutating func removeDuplicates() {
         self = Array(Set(self))
     }
     
     
     /// Removes the duplicates from the array. The function affects the order of the array.
-    func duploMap() -> Array<Element> {
+    public func duploMap() -> Array<Element> {
         return Array(Set(self))
     }
     
     /// Non-duplicates from the array. The function affects the order of the array.
-    var nonDuplicates:Array<Element> {
+    public var nonDuplicates:Array<Element> {
         return Array(Set(self))
     }
     
     
-    static func ====(left:Array<Element>, right:Array<Element>) -> Bool {
+    public static func ====(left:Array<Element>, right:Array<Element>) -> Bool {
         
         let set1 = Set(arrayLiteral: left)
         let set2 = Set(arrayLiteral: right)
