ae05ae72ead7bb1145361a2bd3d60675818ec789
Jan Rajtr <rajtrj@cngroup.dk>
2019-09-29 15:24:18 +0200

    - some stuff for date

--- Sources/UsefulThings/Extensions/Date.swift
+++ Sources/UsefulThings/Extensions/Date.swift
@@ -28,6 +28,27 @@ public enum DateLocale:String {
     case enUsPosix = "en_US_POSIX"
 }
 
+public extension Date {
+    static var yesterday: Date { return Date().dayBefore }
+    static var tomorrow:  Date { return Date().dayAfter }
+    var dayBefore: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    var dayAfter: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    /*
+    var month: Int {
+        return Calendar.current.component(.month,  from: self)
+    }
+    var isLastDayOfMonth: Bool {
+        return dayAfter.month != month
+    }
+     */
+}
 
 public extension Date {
     
