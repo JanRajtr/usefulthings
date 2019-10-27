0ab99b8a347332ea6a423a69f63541911abbbc9d
Jan Rajtr <rajtrj@cngroup.dk>
2019-10-27 11:24:43 +0100

    - types now may be wrapped to KleisliParameter so the kleisli will work with multiple paramteterd functions

--- Sources/UsefulThings/UsefulWhatever/Kleisli.swift
+++ Sources/UsefulThings/UsefulWhatever/Kleisli.swift
@@ -7,21 +7,32 @@
 
 import Foundation
 
-infix operator >=>: AdditionPrecedence
+infix operator >>->>: AdditionPrecedence
 public typealias KleisliRequestCompletion<ResponseType> = (ResponseType) -> Void
 public typealias KleisliRequest<T, U> = (T, @escaping KleisliRequestCompletion<U>) -> Void
 
 /// This operator chains asynchronous operations in so-called Kleisli composition.
 /// - Parameter f: first function
 /// - Parameter g: second function
-public func >=> <T, U, V> (f: @escaping KleisliRequest<T, Result<U,Error>>, g: @escaping KleisliRequest<U, Result<V,Error>>) -> KleisliRequest<T, Result<V,Error>> {
+/// - See Also: https://github.com/typelift/Operadics
+/// <A, B, C>(A -> F<B>, B -> F<C>) -> A -> F<C>
+public func >>->> <T:KleisliParameter, U:KleisliParameter, V:KleisliParameter> (f: @escaping KleisliRequest<T, Result<U,Error>>, g: @escaping KleisliRequest<U, Result<V,Error>>) -> KleisliRequest<T, Result<V,Error>> {
     return { (input, combineCompletion) in
         
         f(input) { (u: Result<U,Error>) in
             switch u {
-            case .success(let unwrappedU): g(unwrappedU) { (v: Result<V,Error>) in combineCompletion(v) }
-            case .failure(let error): combineCompletion(.failure(error))
+            case .success(let unwrappedU):
+                g(unwrappedU) { (v: Result<V,Error>) in
+                    combineCompletion(v)
+                }
+            case .failure(let error):
+                combineCompletion(.failure(error))
             }
         }
     }
 }
+
+public protocol KleisliParameter {
+    
+}
+
