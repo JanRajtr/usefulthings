b18666fac7e2fb7e93214031781f00c1851bb762
Jan Rajtr <rajtrj@cngroup.dk>
2019-10-28 09:03:34 +0100

    - fixed kleisli

--- Sources/UsefulThings/UsefulWhatever/Kleisli.swift
+++ Sources/UsefulThings/UsefulWhatever/Kleisli.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 infix operator >>->>: AdditionPrecedence
-public typealias KleisliRequestCompletion<ResponseType> = (ResponseType) -> Void
+public typealias KleisliRequestCompletion<T> = (Result<T,Error>) -> Void
 public typealias KleisliRequest<T, U> = (T, @escaping KleisliRequestCompletion<U>) -> Void
 
 /// This operator chains asynchronous operations in so-called Kleisli composition.
@@ -16,7 +16,8 @@ public typealias KleisliRequest<T, U> = (T, @escaping KleisliRequestCompletion<U
 /// - Parameter g: second function
 /// - See Also: https://github.com/typelift/Operadics
 /// <A, B, C>(A -> F<B>, B -> F<C>) -> A -> F<C>
-public func >>->> <T:KleisliParameter, U:KleisliParameter, V:KleisliParameter> (f: @escaping KleisliRequest<T, Result<U,Error>>, g: @escaping KleisliRequest<U, Result<V,Error>>) -> KleisliRequest<T, Result<V,Error>> {
+public func >>->> <T, U, V> (f: @escaping KleisliRequest<T, U>,
+                             g: @escaping KleisliRequest<U, V>) -> KleisliRequest<T, V> {
     return { (input, combineCompletion) in
         
         f(input) { (u: Result<U,Error>) in
@@ -31,8 +32,3 @@ public func >>->> <T:KleisliParameter, U:KleisliParameter, V:KleisliParameter> (
         }
     }
 }
-
-public protocol KleisliParameter {
-    
-}
-
